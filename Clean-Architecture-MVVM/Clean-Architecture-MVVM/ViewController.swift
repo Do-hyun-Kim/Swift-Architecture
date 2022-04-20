d14a9524792c56caaf70ba2be5cb10b74e5e36b9
Do-hyun-Kim <dohyun8032@gmail.com>
2022-04-20 13:12:23 +0900

    Clean-Architecture-MVVM 예제 파일 Upload

---
+++ Clean-Architecture-MVVM/Clean-Architecture-MVVM/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Clean-Architecture-MVVM
+//
+//  Created by Kim dohyun on 2022/04/20.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ Clean-Architecture-MVVM/Clean-Architecture-MVVMTests/Clean_Architecture_MVVMTests.swift
@@ -0,0 +1,33 @@
+//
+//  Clean_Architecture_MVVMTests.swift
+//  Clean-Architecture-MVVMTests
+//
+//  Created by Kim dohyun on 2022/04/20.
+//
+
+import XCTest
+@testable import Clean_Architecture_MVVM
+
+class Clean_Architecture_MVVMTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
